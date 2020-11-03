337489609a09621f343a1cbaec9240cf3d5efcb4
Daniellayeghi <Daniellayeghi@gmail.com>
2020-11-03 12:57:58 +0000

    Correct value iteration

--- value_gradient/val_gradient.py
+++ value_gradient/val_gradient.py
@@ -1,70 +1,102 @@
-from mujoco_py import load_model_from_path, MjSim, MjViewer
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 from collections import namedtuple
+import matplotlib.pyplot as plt
+import numpy as np
+from mujoco_py import load_model_from_path, MjSim
+from scipy import interpolate
 
 State = namedtuple('State', 'time qpos qvel act udd_state')
+np.set_printoptions(threshold=np.inf)
 
 
 class QRCost(object):
-    def __init__(self, q: np.array, r: np.array):
+    def __init__(self, q: np.array, r: np.array, ref: np.array):
         self._Q = q
         self._R = r
-        self._ref = np.array([1.46152155e-17, 0.00000000e+00, 1.19342291e-01])
+        self._ref = ref
 
     def cost_function(self, state, ctrl: np.array):
-        err = -self._ref + state[1]
+        err = self._ref - state
         state_cost = err.dot(self._Q).dot(err)
         ctrl_reg   = ctrl.dot(self._R).dot(ctrl)
         return state_cost + ctrl_reg
 
 
 class ValueGradient(object):
-    def __init__(self, state_size: int, cost_function, discrete_num: int):
+    def __init__(self, states: np.array, ctrls: np.array):
         try:
             # Assumes state as 2 dimensional
-            assert(state_size <= 2)
+            assert(states.shape[0] <= 2)
         except AssertionError as error:
             print(error)
 
-        self._cost_func = cost_function
-        self._values    = np.ones([discrete_num, discrete_num]) * -np.inf
+        self._states = states
+        self._ctrls  = ctrls
+        self._values = np.zeros([states.shape[1], states.shape[1]])
 
-    def solve_disc_value_iteration(self, model: MjSim, states, ctrls):
+    def solve_disc_value_iteration(self, model: MjSim, cost_func):
         [row, col] = np.shape(states)
-        for s_1 in range(col):
-            for s_2 in range(col):
-                for ctrl in range(ctrls.shape[0]):
-                    model.set_state(
-                        State(time=0, qpos=np.array([states[0][s_1]]), qvel=np.array([states[1][s_2]]), act=0,
-                              udd_state={})
-                    )
-                    model.data.ctrl[0] = ctrls[ctrl]
-                    model.step()
-                    cost = self._cost_func(model.data.xipos, model.data.ctrl)
-                    if cost > self._values[s_1][s_2]:
-                        self._values[s_1][s_2] = cost
-
+        for iter in range(5):
+            print(f"iteration: {iter}")
+            for s_1 in range(col):
+                for s_2 in range(col):
+                    for ctrl in range(self._ctrls.shape[0]):
+                        # reinitialise the state
+                        model.set_state(
+                            State(time=0, qpos=np.array([states[0][s_1]]), qvel=np.array([states[1][s_2]]), act=0,
+                                  udd_state={})
+                        )
+                        model.data.ctrl[0] = self._ctrls[ctrl]
+                        model.step()
+                        # solve for the instantaneous cost and interpolate the value at the next state
+                        value_curr = cost_func(
+                            np.append(model.data.xipos[1], model.data.qvel[0]), model.data.ctrl
+                        ) + interpolate.interp2d(
+                            self._states[0][:], self._states[1][:], self._values, kind='linear'
+                        )(model.data.qpos[0], model.data.qvel[0])
+                        # Hacky fix for the first iteration of vi
+                        if ctrl == 0 and iter == 0:
+                            self._values[s_1][s_2] = value_curr
+                        if value_curr < self._values[s_1][s_2]:
+                            # print(f"Difference = {value_curr - self._values[s_1][s_2]} index {s_1}{s_2}")
+                            self._values[s_1][s_2] = value_curr[0]
         return self._values
 
 
 if __name__ == "__main__":
-    cost = QRCost(np.diagflat(np.array([100, 100, 100 * 0.01])), np.diagflat(np.array([10])))
-    disc_state = 100
-    disc_ctrl  = 200
-    pos_arr = (np.linspace(-5, 5, disc_state))
-    vel_arr = (np.linspace(-5, 5, disc_state))
-    states  = np.array((pos_arr, vel_arr))
-    vg = ValueGradient(2, cost.cost_function, disc_ctrl)
-    mdl = load_model_from_path("/home/daniel/Repos/mujoco_py_test/xmls/Pendulum.xml")
+    # Setup quadratic cost
+    cost = QRCost(
+        np.diagflat(np.array([500, 0, 500, 500 * 0.05])),
+        np.diagflat(np.array([1])),
+        np.array([1.46152155e-17, 0.00000000e+00, 1.19342291e-01, 0])
+    )
+
+    # Setup value iteration
+    disc_state = 25
+    disc_ctrl = 25
+    pos_arr = (np.linspace(-np.pi, np.pi*3, disc_state))
+    vel_arr = (np.linspace(-np.pi, np.pi, disc_state))
+    states = np.array((pos_arr, vel_arr))
+    ctrls = np.linspace(-1, 1, disc_ctrl)
+    vg = ValueGradient(states, ctrls, )
+
+    # Load environment
+    mdl = load_model_from_path("../xmls/Pendulum.xml")
     sim = MjSim(mdl)
-    values = vg.solve_disc_value_iteration(sim, states, np.linspace(-1, 1, disc_ctrl))
+
+    # Solve value iteration
+    values = vg.solve_disc_value_iteration(sim, cost.cost_function)
+    min = np.unravel_index(values.argmin(), values.shape)
+    print(f"The min value is at pos {pos_arr[min[0]]} and vel {vel_arr[min[1]]}")
+
+    # Plot the structure of cost to go
     [P, V] = np.meshgrid(pos_arr, vel_arr)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     ax.plot_surface(P, V, values, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
     ax.set_title('surface')
+    ax.set_xlabel('Pos')
+    ax.set_ylabel('Vel')
+    ax.set_zlabel('Value')
     plt.show()
 
 
