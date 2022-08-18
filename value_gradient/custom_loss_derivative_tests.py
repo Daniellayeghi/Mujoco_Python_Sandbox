Correct gradients

value_gradient/custom_loss_derivative_tests.py
@@ -15,16 +15,33 @@ m = mujoco.MjModel.from_xml_path(
 
 batch_size = 1
 DataParams = namedtuple('DataParams', 'n_full_state, n_state, n_pos, n_vel, n_ctrl, n_desc, idx_g_act, n_batch')
-d_params = DataParams(3, 2, 1, 1, 1, 2, batch_size)
+d_params = DataParams(3, 2, 1, 1, 1, 2, [1, 2], batch_size)
 d = mujoco.MjData(m)
 bo = MjBatchOps(m, d_params)
-x_full = torch.zeros(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double).requires_grad_()
+x_full = torch.randn(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double).requires_grad_() * 0.01
+x_full[0, :] = torch.tensor([0.5442, -1.03142738, -0.02782536])
+x_full_np = tensor_to_np(x_full)
 u_star = torch.zeros(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double)
+
+
+class finv(Function):
+    @staticmethod
+    def forward(ctx, x_full):
+        x_full_cpu = x_full.cpu()
+        x_full_np = tensor_to_np(x_full)
+        qfrcs = bo.b_qfrcs(x_full_np)
+        dfinvdx = bo.b_dfinvdx_full(x_full_np)
+        ctx.save_for_backward(np_to_tensor(dfinvdx))
+        return np_to_tensor(qfrcs)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        dfinvdx = ctx.saved_tensors[0]
+        return grad_output * dfinvdx
 
 
 if __name__ == "__main__":
     loss_functions.set_batch_ops__(bo)
-    torch.autograd.gradcheck(ctrl_clone_loss.apply, (x_full, u_star))
-    torch.autograd.gradcheck(ctrl_effort_loss.apply, x_full)
 
-
+    torch.autograd.gradcheck(ctrl_effort_loss.apply, (x_full), rtol=1e-1, atol=1e-1)
+    torch.autograd.gradcheck(ctrl_clone_loss.apply, (x_full, u_star), rtol=1e-1, atol=1e-1)

value_gradient/loss_functions.py
@@ -31,10 +31,13 @@ class value_lie_loss(Function):
     @staticmethod
     def forward(ctx, x, u):
         ctx.constant = u
-        x_full_cpu, u_cpu = x.cpu(), u.cpu()
-        x_full_np = x.detach().numpy().astype('float64')
-        u_np = u.detach().numpy().astype('float64')
-        dvdx_np = _value_net_.dvdx(x).detach().numpy().astype('float64')
+        x_cpu, u_cpu = x.cpu(), u.cpu()
+        x_np = tensor_to_np(x)
+        u_np = tensor_to_np(u)
+        dvdx_np = tensor_to_np(_value_net_.dvdx(x))
+        gu = _batch_op_.b_gu(u_np)
+        dxdt = _batch_op_.b_dxdt(x)
+        return dvdx_np * gu + dvdx_np * dxdt
 
 
     @staticmethod
@@ -42,24 +45,27 @@ class value_lie_loss(Function):
         pass
 
 
+
+
+
 class ctrl_effort_loss(Function):
     @staticmethod
     def forward(ctx, x_full):
         x_full_cpu = x_full.cpu()
-        x_full_np = x_full.detach().numpy().astype('float64')
+        x_full_np = tensor_to_np(x_full)
         qfrcs = _batch_op_.b_qfrcs(x_full_np)
         ctx.save_for_backward(x_full_cpu, np_to_tensor(qfrcs))
-        loss = torch.square(torch.tensor(qfrcs, device=device, dtype=dtype))
+        loss = torch.sum(torch.square(torch.tensor(qfrcs, device=device, dtype=dtype)))
         return loss
 
     @staticmethod
     def backward(ctx, grad_output):
         x_full, qfrcs = ctx.saved_tensors
-        frcs_np = qfrcs.detach().numpy().astype('float64')
-        x_full_np = x_full.detach().numpy().astype('float64')
+        frcs_np = tensor_to_np(qfrcs)
+        x_full_np = tensor_to_np(x_full)
         dfinvdx = _batch_op_.b_dfinvdx_full(x_full_np)
         grad_1 = grad_output * 2 * frcs_np * dfinvdx.reshape(
-            _batch_op_.params.n_vel, _batch_op_.params.n_full_state
+            _batch_op_.params.n_batch, _batch_op_.params.n_vel, _batch_op_.params.n_full_state
         )
         return grad_1
 
@@ -70,10 +76,10 @@ class ctrl_clone_loss(Function):
         ctx.constant = u_star
         x_full_cpu = x_full.cpu()
         u_star_cpu = u_star.cpu()
-        x_full_np = x_full.detach().numpy().astype('float64')
+        x_full_np = tensor_to_np(x_full)
         qfrcs = _batch_op_.b_qfrcs(x_full_np)
         ctx.save_for_backward(x_full_cpu, np_to_tensor(qfrcs), u_star_cpu)
-        return torch.square(u_star - qfrcs)
+        return torch.sum(torch.square(u_star - qfrcs))
 
     @staticmethod
     def backward(ctx, grad_output):

value_gradient/tensor_derivative_tests.py
@@ -1,33 +1,49 @@
 import numpy as np
-from mujoco import derivative
+from scipy.optimize import approx_fprime
 from utilities.mj_utils import MjBatchOps
 from collections import namedtuple
+from utilities.torch_utils import *
 import mujoco
 import torch
 import cProfile
 
 m = mujoco.MjModel.from_xml_path(
-    "/home/daniel/Repos/OptimisationBasedControl/models/cartpole.xml"
+    "/home/daniel/Repos/OptimisationBasedControl/models/doubleintegrator.xml"
 )
 
-batch_size = 2
-DataParams = namedtuple('DataParams', 'n_full_state, n_state, n_pos, n_vel, n_ctrl, n_desc, n_batch')
-d_params = DataParams(6, 4, 2, 2, 1, 2, batch_size)
+batch_size = 1
+DataParams = namedtuple('DataParams', 'n_full_state, n_state, n_pos, n_vel, n_ctrl, n_desc, idx_g_act, n_batch')
+d_params = DataParams(3, 2, 1, 1, 1, 2, [1, 2], batch_size)
 d = mujoco.MjData(m)
 bo = MjBatchOps(m, d_params)
 
+x_full = torch.randn(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double).requires_grad_() * 0.01
+x_full[0, :] = torch.tensor([0.5442, -1.03142738, -0.02782536])
+u_star = torch.zeros(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double)
+
+
+def f_inv(xf):
+    d.qpos = xf[:d_params.n_pos]
+    d.qvel = xf[d_params.n_pos:d_params.n_state]
+    d.qacc = xf[d_params.n_state:]
+    mujoco.mj_inverse(m, d)
+    return d.qfrc_inverse
+
+
+def effort_loss(xf):
+    d.qpos = xf[:d_params.n_pos]
+    d.qvel = xf[d_params.n_pos:d_params.n_state]
+    d.qacc = xf[d_params.n_state:]
+    mujoco.mj_inverse(m, d)
+    return np.sum(np.square(d.qfrc_inverse))
+
 
 def main():
-
-    x = torch.zeros(batch_size, d_params.n_state)
-    full_x = torch.zeros(batch_size, d_params.n_state + d_params.n_vel)
-    u = torch.zeros(batch_size, d_params.n_ctrl)
-
     res_dfdx = bo.b_dfdx(x, u)
     res_dfdu = bo.b_dfdu(x, u)
-    res_dfinvdx = bo.b_dfinvdx_full(full_x)
+    res_dfinvdx = bo.b_dfinvdx_full(x_full)
     res_dfdt = bo.b_dxdt(x, u)
-    res_qfrc = bo.b_qfrcs(full_x)
+    res_qfrc = bo.b_qfrcs(x_full)
 
     print(res_dfdx[0].reshape((d_params.n_state, d_params.n_state)))
     print(res_dfdu[0].reshape((d_params.n_state, d_params.n_ctrl)))
@@ -38,6 +54,16 @@ def main():
 
 
 if __name__ == "__main__":
-    cProfile.run('main()')
+    x_full_np = tensor_to_np(x_full.flatten())
+    J_ctrl = approx_fprime(x_full_np, f_inv, 1e-6)
+    J_effort = approx_fprime(x_full_np, effort_loss, 1e-6)
+
+    print(f"Finv Scipy deriv:\n{J_ctrl}, for inv {f_inv(x_full_np)}")
+    print(f"Finv FD deriv:\n{bo.b_dfinvdx_full(x_full)}")
+
+    print(f"Effort Scipy deriv:\n{J_effort}, for loss {effort_loss(x_full_np)}")
+    print(f"Effort FD deriv:\n{2 * bo.b_qfrcs(x_full) * bo.b_dfinvdx_full(x_full)}")
+
+    # cProfile.run('main()')
 
 

value_gradient/utilities/mj_utils.py
@@ -10,47 +10,51 @@ class MjBatchOps:
     def __init__(self, m, params):
         self.__model = m
         self.params = params
-        self.__data = mujoco.MjData(m)
-        self.__dfdx = MjDerivative(m, self.__data, MjDerivativeParams(1e-6, Wrt.State, Mode.Fwd))
-        self.__dfdu = MjDerivative(m, self.__data, MjDerivativeParams(1e-6, Wrt.Ctrl, Mode.Fwd))
-        self.__dfinvdx = MjDerivative(m, self.__data, MjDerivativeParams(1e-6, Wrt.State, Mode.Inv))
+        self._data = mujoco.MjData(m)
+        self._dfdx = MjDerivative(m, self._data, MjDerivativeParams(1e-6, Wrt.State, Mode.Fwd))
+        self._dfdu = MjDerivative(m, self._data, MjDerivativeParams(1e-6, Wrt.Ctrl, Mode.Fwd))
+        self._dfinvdx = MjDerivative(m, self._data, MjDerivativeParams(1e-6, Wrt.State, Mode.Inv))
 
-        self.__qfrc_t = torch.zeros(
+        self._qfrc_t = torch.zeros(
             (self.params.n_batch, self.params.n_vel), dtype=torch.double
         ).requires_grad_()
 
-        self.__dxdt_t = torch.zeros(
+        self._dxdt_t = torch.zeros(
             (self.params.n_batch, self.params.n_vel * 2), dtype=torch.double
         ).requires_grad_()
 
-        self.__dfinvdx_t = torch.zeros(
+        self._dfinvdx_t = torch.zeros(
             (self.params.n_batch, self.params.n_full_state * self.params.n_vel), dtype=torch.double
         ).requires_grad_()
 
-        self.__dfdx_t = torch.zeros(
+        self._dfdx_t = torch.zeros(
             (self.params.n_batch, self.params.n_state**2), dtype=torch.double
         ).requires_grad_()
 
-        self.__dfdu_t = torch.zeros(
+        self._dfdu_t = torch.zeros(
             (self.params.n_batch, self.params.n_ctrl*self.params.n_state), dtype=torch.double
         ).requires_grad_()
 
-        self.__gu_t = torch.zeros(
+        self._gu_t = torch.zeros(
             (self.params.n_batch, self.params.n_state, self.params.n_ctrl), dtype=torch.double
-        ).requires_grad_()
+        )
 
-        self.__u_t = torch.zeros(
+        self._u_t = torch.zeros(
             (self.params.n_batch, self.params.n_ctrl), dtype=torch.double
         ).requires_grad_()
 
-        self.__gu_t[:, self.params.g_act_idx[0]:self.params.g_act_idx[1], self.params.n_ctrl] = 1
+        self._gu_t[:, self.params.idx_g_act[0]:self.params.idx_g_act[1], :] = 1
+        self._gu_t.requires_grad_()
 
-        self.__qfrc_np = self.__qfrc_t.detach().numpy().astype('float64')
-        self.__dfinvdx_np = self.__dfinvdx_t.detach().numpy().astype('float64')
-        self.__dxdt_np = self.__dxdt_t.detach().numpy().astype('float64')
-        self.__dfdx_np = self.__dfdx_t.detach().numpy().astype('float64')
-        self.__dfdu_np = self.__dfdu_t.detach().numpy().astype('float64')
-        self.__gu_np = self.__gu_t.detach().numpy().astype('float64')
+        self._qfrc_np = self._qfrc_t.detach().numpy().astype('float64')
+        self._dfinvdx_np = self._dfinvdx_t.detach().numpy().astype('float64')
+        self._dxdt_np = self._dxdt_t.detach().numpy().astype('float64')
+        self._dfdx_np = self._dfdx_t.detach().numpy().astype('float64')
+        self._dfdu_np = self._dfdu_t.detach().numpy().astype('float64')
+        self._gu_np = self._gu_t.detach().numpy().astype('float64')
+
+    def _reset_inverse(self):
+        self._data.qfrc_inverse[:] = 0
 
     @staticmethod
     def _get_result(operation, tensor_flag):
@@ -60,20 +64,22 @@ class MjBatchOps:
             return operation.func().flatten()
 
     def _mj_set_ctrl(self, u):
-        self.__data.ctrl = u
+        self._data.ctrl = u
 
     def _mj_set_x_full_decomp(self, pos, vel, acc):
-        self.__data.qpos, self.__data.qvel, self.__data.qacc = pos, vel, acc
+        self._data.qpos, self._data.qvel, self._data.qacc = pos, vel, acc
 
     def _mj_set_state(self, pos, vel):
-        self.__data.qpos, self.__data.qvel = pos, vel
+        self._data.qpos, self._data.qvel = pos, vel
 
     def _mj_set_x_full(self, x):
+        x = x.flatten()
         self._mj_set_x_full_decomp(
             x[:self.params.n_pos], x[self.params.n_pos:self.params.n_state], x[self.params.n_state:]
         )
 
     def _mj_set_x(self, x):
+        x = x.flatten()
         self._mj_set_state(x[:self.params.n_pos], x[self.params.n_pos:self.params.n_state])
 
     def _mj_set_x_decomp_ctrl(self, pos, vel, u):
@@ -85,16 +91,16 @@ class MjBatchOps:
         self._mj_set_state(x[:self.params.n_pos], x[self.params.n_pos:self.params.n_state])
 
     def f(self, tensor=True):
-        mujoco.mj_step(self.__model, self.__data)
-        res = np.hstack((self.__data.qvel, self.__data.qacc)).flatten()
+        mujoco.mj_step(self.__model, self._data)
+        res = np.hstack((self._data.qvel, self._data.qacc)).flatten()
         if tensor:
             return torch.Tensor(res)
         else:
             return res
 
     def finv(self, tensor=True):
-        mujoco.mj_inverse(self.__model, self.__data)
-        res = self.__data.qfrc_inverse.flatten()
+        mujoco.mj_inverse(self.__model, self._data)
+        res = self._data.qfrc_inverse.flatten()
         if tensor:
             return torch.Tensor(res)
         else:
@@ -102,7 +108,7 @@ class MjBatchOps:
 
     def b_qfrc_decomp(self, pos, vel, acc):
         tensor = type(pos) is torch.Tensor
-        res = self.__qfrc_t if tensor else self.__qfrc_np
+        res = self._qfrc_t if tensor else self._qfrc_np
         with torch.no_grad():
             for i in range(pos.shape[0]):
                 self._mj_set_x_full_decomp(pos[i, :], vel[i, :], acc[i, :])
@@ -111,16 +117,18 @@ class MjBatchOps:
 
     def b_qfrcs(self, x_full):
         tensor = type(x_full) is torch.Tensor
-        res = self.__qfrc_t if tensor else self.__qfrc_np
+        res = self._qfrc_t if tensor else self._qfrc_np
         with torch.no_grad():
             for i in range(x_full.shape[0]):
                 self._mj_set_x_full(x_full[i, :])
                 res[i, :] = self.finv(tensor)
+
+        self._reset_inverse()
         return res
 
     def b_dxdt_decomp(self, pos, vel, u):
         tensor = type(pos) is torch.Tensor
-        res = self.__dfdx_t if tensor else self.__dfdx_np
+        res = self._dfdx_t if tensor else self._dfdx_np
         with torch.no_grad():
             for i in range(pos.shape[0]):
                 self._mj_set_x_decomp_ctrl(pos[i, :], vel[i, :], u[i, :])
@@ -129,7 +137,7 @@ class MjBatchOps:
 
     def b_dxdt(self, x, u):
         tensor = type(x) is torch.Tensor
-        res = self.__dxdt_t if tensor else self.__dxdt_np
+        res = self._dxdt_t if tensor else self._dxdt_np
         with torch.no_grad():
             for i in range(x.shape[0]):
                 self._mj_set_x_ctrl(x[i, :], u[i, :])
@@ -138,56 +146,56 @@ class MjBatchOps:
 
     def b_dfinvdx_full(self, x_full):
         tensor = type(x_full) is torch.Tensor
-        res = self.__dfinvdx_t if tensor else self.__dfinvdx_np
+        res = self._dfinvdx_t if tensor else self._dfinvdx_np
         with torch.no_grad():
             for i in range(x_full.shape[0]):
                 self._mj_set_x_full(x_full[i, :])
-                res[i, :] = self._get_result(self.__dfinvdx, tensor)
+                res[i, :] = self._get_result(self._dfinvdx, tensor)
         return res
 
     def b_dfinvdx_full_decomp(self, pos, vel, acc):
         tensor = type(pos) is torch.Tensor
-        res = self.__dfinvdx_t if tensor else self.__dfinvdx_np
+        res = self._dfinvdx_t if tensor else self._dfinvdx_np
         with torch.no_grad():
             for i in range(pos.shape[0]):
                 self._mj_set_x_full_decomp(pos[i, :], vel[i, :], acc[i, :])
-                res[i, :] = self._get_result(self.__dfinvdx, tensor)
+                res[i, :] = self._get_result(self._dfinvdx, tensor)
         return  res
 
     def b_dfdx(self, x, u):
         tensor = type(x) is torch.Tensor
         with torch.no_grad():
-            res = self.__dfdx_t if tensor else self.__dfdx_np
+            res = self._dfdx_t if tensor else self._dfdx_np
             for i in range(x.shape[0]):
                 self._mj_set_x_ctrl(x[i, :], u[i, :])
-                res[i, :] = self._get_result(self.__dfdx, tensor)
+                res[i, :] = self._get_result(self._dfdx, tensor)
         return res
 
     def b_dfdx_decomp(self, pos, vel, u):
         tensor = type(pos) is torch.Tensor
-        res = self.__dfdx_t if tensor else self.__dfdx_np
+        res = self._dfdx_t if tensor else self._dfdx_np
         with torch.no_grad():
             for i in range(pos.shape[0]):
                 self._mj_set_x_decomp_ctrl(pos[i, :], vel[i, :], u[i, :])
-                res[i, :] = self._get_result(self.__dfdx, tensor)
+                res[i, :] = self._get_result(self._dfdx, tensor)
         return res
 
     def b_dfdu(self, x, u):
         tensor = type(x) is torch.Tensor
-        res = self.__dfdu_t if tensor else self.__dfdu_np
+        res = self._dfdu_t if tensor else self._dfdu_np
         with torch.no_grad():
             for i in range(x.shape[0]):
                 self._mj_set_x_ctrl(x[i, :], u[i, :])
-                res[i, :] = self._get_result(self.__dfdu, tensor)
+                res[i, :] = self._get_result(self._dfdu, tensor)
         return res
 
     def b_gu(self, u):
         tensor = type(u) is torch.Tensor
-        res = self.__gu_t if tensor else self.__gu_np
-        self.__u_t = torch.tensor(u)
-        self.__u_t.reshape((self.params.n_batch, self.params.n_ctrl, 1))
+        res = self._gu_t
+        self._u_t = torch.tensor(u)
+        self._u_t.reshape((self.params.n_batch, self.params.n_ctrl, 1))
         with torch.no_grad:
-            return torch.bmm(res, self.__u_t)
+            return torch.bmm(res, self._u_t) if tensor else torch.bmm(res, self._u_t).numpy()
 
 
 