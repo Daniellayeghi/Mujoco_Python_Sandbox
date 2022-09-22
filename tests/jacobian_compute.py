correct hessian computation

tests/jacobian_compute.py
@@ -13,20 +13,20 @@ dfdu = MjDerivative(m, d, MjDerivativeParams(1e-6, Wrt.Ctrl, Mode.Fwd))
 
 def copy_data(d_src, d_target):
     d_target.qpos = d_src.qpos
-    d_target.qvel = d_target.qvel
-    d_target.qacc = d_target.qacc
-    d_target.qfrc_applied = d_target.qfrc_applied
-    d_target.xfrc_applied = d_target.xfrc_applied
-    d_target.ctrl = d_target.ctrl
+    d_target.qvel = d_src.qvel
+    d_target.qacc = d_src.qacc
+    d_target.qfrc_applied = d_src.qfrc_applied
+    d_target.xfrc_applied = d_src.xfrc_applied
+    d_target.ctrl = d_src.ctrl
     mujoco.mj_forward(m, d_target)
 
 
 def fwd_u(u):
-    d_cp.ctrl[1] = u
+    d_cp.ctrl = u
     mujoco.mj_step(m, d_cp)
-    res = np.copy(d_cp.qpos)
+    res = np.vstack((d_cp.qpos, d_cp.qvel))
     copy_data(d, d_cp)
-    return res[0]
+    return res.flatten()
 
 
 def fwd_x(x):
@@ -37,22 +37,31 @@ def fwd_x(x):
 
 
 if __name__ == "__main__":
-    d.qpos, d.qvel, n_full, epsilon = 0, 0, m.nq * 3, 1e-6
-    d_cp.qpos, d_cp.qvel, = 0, 0
+    d.qpos, d.qvel, n_full, epsilon = [10, 10], [0, 0], m.nq * 2, 1e-6
+    d_cp.qpos, d_cp.qvel = [10, 10], [0, 0]
 
     u = np.zeros_like(d.ctrl)
     dxdu_mine = dfdu.func()
 
-    d = mujoco.MjData(m)
-    d.qpos, d.qvel, n_full, epsilon = 0, 0, m.nq * 3, 1e-6
-    d_cp.qpos, d_cp.qvel, = 0, 0
+    jac_op = nd.Jacobian(
+        lambda x: fwd_u(x), method='forward', step=epsilon, order=0, step_ratio=1
+    )
 
-    jac_op = nd.Jacobian(lambda x: fwd_u(x), method='forward', step=epsilon, order=1, step_ratio=1)
-    dxdu_nd = jac_op(0)
+    dxdu_nd = jac_op(u.T)
+    print(f"[dxdu Error]: {np.square(np.sum(dxdu_nd - dxdu_mine))} \n")
 
-    print(f"[dxdu Error]: {np.square(np.sum(dxdu_nd[:-1] - dxdu_mine))}")
+    u_rand = np.array([0, 0])
 
-    hess_op = [nd.Hessian(lambda x: fwd_u(x)[m], method='forward') for m in range(n_full)]
-    dx2_d2u = np.vstack([hess_op[i](u) for i in range(len(hess_op))])
+    hess_ops = []
+    for i in range(n_full):
+        op = nd.Hessian(
+            lambda u: fwd_u(u)[i], method='forward', step=epsilon, order=0, step_ratio=0
+        )
+        hess_ops.append(op)
 
-    print(f"dx2_d2u: {dx2_d2u} and gauss dx2_d2u: {np.outer(2 * dxdu_mine.T, dxdu_mine)}")
+    dx2_d2u = []
+    for i in range(len(hess_ops)):
+        dx2_d2u.append(hess_ops[i](u_rand))
+
+    dx2_d2u = np.vstack(dx2_d2u)
+    print(f"dx2_d2u:\n {dx2_d2u} \n gauss:\n {dxdu_mine.T @ dxdu_mine}")