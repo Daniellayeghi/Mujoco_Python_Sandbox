a161d4b6f6e49519a4228b7ccf9be7741abaa8a8
Daniellayeghi <Daniellayeghi@gmail.com>
2022-11-08 17:14:49 +0000

    Corrections to integral

--- value_gradient/value_projection.m
+++ value_gradient/value_projection.m
@@ -17,26 +17,26 @@ VALUES = ones(size(POS));
 CTRLS = ones(size(POS));
 
 %% Compute values
-for i = 1:length(poses)
-    expr1 =  subs(v(1, i), q, poses(1, i));
-    expr2 =  subs(pi, q, poses(1, i));
-    for j = 1:length(vels)
-        VALUES(i, j) = subs(expr1, qd, vels(1, j));
-        CTRLS(i, j) = subs(expr2, qd, vels(1, j));
-    end
-end
-
-[vq, vqd] = gradient(VALUES);
-save(filename, 'VALUES');
+% for i = 1:length(poses)
+%     expr1 =  subs(v(1, i), q, poses(1, i));
+%     expr2 =  subs(pi, q, poses(1, i));
+%     for j = 1:length(vels)
+%         VALUES(i, j) = subs(expr1, qd, vels(1, j));
+%         CTRLS(i, j) = subs(expr2, qd, vels(1, j));
+%     end
+% end
+% 
+% [vq, vqd] = gradient(VALUES);
+% save(filename, 'VALUES');
 
 %% Projection
 close all
 VALUES = load(filename).VALUES;
 [vq, vqd] = gradient(VALUES);
 
-alpha = 1;
+alpha = 50;
+
 % Cost function and their derivatives
-relu = @(x)(if (x > 0) x = x; else x = 0; end)
 f = @(q, qd, qdd)([qd; qdd]);
 v = @(q, qd)(sqrt(3) * q^2 + 2 * q * qd + sqrt(3) * qd^2);
 fcost = @(q, qd, u)(q^2 + qd^2 + u^2);
@@ -46,14 +46,14 @@ real_hessian  = [2*3^(1/2), 2; 2, 2*3^(1/2)];
 sqr_norm = @(q, qd)(sqrt(v_jac(q, qd)' * v_jac(q, qd) + 1e-6));
 
 % Projection operator
-proj_upper = @(q, qd, dfdt)(dfdt - v_jac(q, qd)/sqr_norm(q, qd) * (v_jac(q, qd)' * dfdt + alpha * (q^2 + qd^2 - dfdt(2)^2)));
+proj_upper = @(q, qd, dfdt)(dfdt - v_jac(q, qd)/sqr_norm(q, qd) * relu((v_jac(q, qd)' * dfdt + alpha * (q^2 + qd^2 - dfdt(2)^2))));
 proj_lower = @(q, qd, dfdt)(dfd - v_jac(q, qd) * v_jac(q, qd)' * dfdt / ...
     v_jac_norm(q, qd));
 
 integrator = @(q, qd, qdd)([q + qd * 0.01; qd + qdd * 0.01]);
 
-init =[0.738493562240430, 0.222405510587575];
-q_t= init(1); qd_t= init(2); qdd_t = 0;
+% init =[0.738493562240430, 0.222405510587575];
+q_t= rand * 2; qd_t= rand * 2; qdd_t = 0;
 q_hat_next = q_t; qd_hat_next = qd_t;
 figure();
 hax1 = axes;
@@ -71,7 +71,7 @@ while sqrt([q_t, qd_t] * [q_t; qd_t]) > 0.1
 
     % compute the next state
     qh_t2 = q_t + qd_t * 0.01; qdh_t2 = qd_t + qdd_t * 0.01;
-    plot(hax1, qh_t2, qdh_t2, '*');
+%     plot(hax1, qh_t2, qdh_t2, '*');
     
     % assemble dxdt and project
     dfdt   = [qd_t; qdd_t];
@@ -80,7 +80,7 @@ while sqrt([q_t, qd_t] * [q_t; qd_t]) > 0.1
 
     pos_buff = [pos_buff; [q_hat_next, qd_hat_next, fnext(2)]];
     % integrate to find next state along V
-    q_hat_next = q_t + fnext(1) * 0.01;
+    q_hat_next = q_t + qd_t * 0.01;
     qd_hat_next = qd_t + fnext(2) * 0.01;
     
     % update state
@@ -105,3 +105,8 @@ hold on;
 plot(ax3, pos_buff(:, 3));
 
 
+function  x = relu(x)
+    if x < 0
+       x = 0;
+    end
+end
