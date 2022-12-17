correct derivatives

value_gradient/tensor_derivative_tests.py
@@ -6,34 +6,30 @@ from utilities.torch_utils import *
 from mujoco import MjData
 import mujoco
 import torch
-from utilities.mujoco_torch import torch_mj_inv, torch_mj_set_attributes
+from torch.autograd.functional import jacobian
+from utilities.mujoco_torch import torch_mj_inv, torch_mj_set_attributes, SimulationParams
 
-m = mujoco.MjModel.from_xml_path("/home/daniel/Repos/OptimisationBasedControl/models/doubleintegrator.xml")
+m = mujoco.MjModel.from_xml_path("/home/daniel/Repos/OptimisationBasedControl/models/doubleintegrator_sparse.xml")
 
-batch_size = 1
-DataParams = namedtuple('DataParams', 'n_full_state, n_state, n_pos, n_vel, n_ctrl, n_desc, idx_g_act, n_batch')
-d_params = DataParams(3, 2, 1, 1, 1, 2, [1, 2], batch_size)
+d_params = SimulationParams(6, 4, 2, 2, 1, 2, 1, 1)
 d = mujoco.MjData(m)
-bo = MjBatchOps(m, d_params)
 
-x_full = torch.randn(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double).requires_grad_() * 0.6
-x_full[0, :] = torch.tensor([0.5442, -1.03142738, -0.02782536])
-u_star = torch.zeros(batch_size, d_params.n_state + d_params.n_vel, dtype=torch.double)
+u_star = torch.zeros(1, d_params.nqva, dtype=torch.double)
 u_star_loss = np.ones_like(d.qfrc_inverse)
 
-torch_mj_set_attributes(m, batch_size)
+torch_mj_set_attributes(m, d_params)
 
 
 def set_xfull(d: MjData, xf):
-    d.qpos = xf[:d_params.n_pos]
-    d.qvel = xf[d_params.n_pos:d_params.n_state]
-    d.qacc = xf[d_params.n_state:]
+    d.qpos = xf[:d_params.nq]
+    d.qvel = xf[d_params.nq:d_params.nqv]
+    d.qacc = xf[d_params.nqv:]
 
 
 def f_inv(xf):
     set_xfull(d, xf)
     mujoco.mj_inverse(m, d)
-    return d.qfrc_inverse
+    return d.qfrc_inverse[1]
 
 
 def effort_loss(xf):
@@ -51,27 +47,15 @@ def clone_loss(xf):
 t_mj_inv = torch_mj_inv.apply
 
 if __name__ == "__main__":
+    x_full = torch.randn(1, 1, 1, d_params.nqva, dtype=torch.double).requires_grad_() * 0.6
+    x_full[0, 0, 0, :] = torch.tensor([-.6, 0, 0, 0, 0, 0])
     x_full_np = tensor_to_np(x_full.flatten())
     J_inv = approx_fprime(x_full_np, f_inv, 1e-6)
     J_effort = approx_fprime(x_full_np, effort_loss, 1e-6)
     J_clone = approx_fprime(x_full_np, clone_loss, 1e-6)
-
     set_xfull(d, x_full_np)
     mujoco.mj_inverse(m, d)
-    print(f"Finv mujoco: \n{d.qfrc_inverse}")
+    dfinv_dx = jacobian(t_mj_inv, x_full)
     qfrc_torch = t_mj_inv(x_full)
-    print(f"Finv torch: \n{qfrc_torch}")
-
-    x_full.retain_grad()
-    qfrc_torch.retain_grad()
-    qfrc_torch.backward()
-
-    print(f"dFinvdx torch deriv:\n{x_full.grad}")
-    print(f"dFinvdx Scipy deriv:\n{J_inv}")
-    print(f"dFinvdx FD deriv:\n{bo.b_dfinvdx_full(x_full)}")
-
-    print(f"Effort Scipy deriv:\n{J_effort}")
-    print(f"Effort FD deriv:\n{2 * bo.b_qfrcs(x_full) * bo.b_dfinvdx_full(x_full)}")
-
-    print(f"Effort Scipy deriv:\n{J_clone}")
-    print(f"Effort FD deriv:\n{2 * (bo.b_qfrcs(x_full).detach().numpy() - u_star_loss) * bo.b_dfinvdx_full(x_full).detach().numpy()}")
+    print(f"Finv torch:\n{qfrc_torch}, Finv:\n{d.qfrc_inverse}")
+    print(f"Finv derivative scipy:\n{J_inv}, Finv derivative torch:\n{dfinv_dx}")