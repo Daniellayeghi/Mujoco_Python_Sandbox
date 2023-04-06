32b4757056bf417c1a0bc299817984bbe521b9a0
Daniel <Daniellayeghi@gmail.com>
2023-04-06 16:29:48 +0100

    Lyapounov train fwd mode

--- value_gradient/tv_value_synthesis_lyap_cartpole.py
+++ value_gradient/tv_value_synthesis_lyap_cartpole.py
@@ -17,7 +17,7 @@ wandb.init(project='cartpole_lyap', entity='lonephd')
 
 sim_params = SimulationParams(6, 4, 2, 2, 2, 1, 20, 100, 0.008)
 cp_params = ModelParams(2, 2, 1, 4, 4)
-max_iter, max_time, alpha, dt, discount, step, scale, mode = 500, 200, .5, 0.008, 20, .005, 10, 'proj'
+max_iter, max_time, alpha, dt, discount, step, scale, mode = 500, 200, .5, 0.008, 20, .005, 10, 'fwd'
 Q = torch.diag(torch.Tensor([50, 25, 0.5, .1])).repeat(sim_params.nsim, 1, 1).to(device)
 R = torch.diag(torch.Tensor([0.0001])).repeat(sim_params.nsim, 1, 1).to(device)
 Qf = torch.diag(torch.Tensor([50, 25, 0.5, .1])).repeat(sim_params.nsim, 1, 1).to(device)
